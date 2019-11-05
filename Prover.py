f50f6458278fcfc4f1e384ff4f6fa6aff16ecc46
kauroy1994 <57400980+kauroy1994@users.noreply.github.com>
2019-11-05 18:49:29 +0530

    Tried simple test to infer rules for man happy

---
+++ Prover.py
@@ -0,0 +1,199 @@
+import string
+from copy import deepcopy
+
+class Proof_node(object):
+
+    def __init__(self,literal,bindings,facts):
+        """proof node during backtracking
+        """
+
+        self.literal = literal
+        self.bindings = bindings
+        self.facts = []
+        for fact in facts:
+            lit_pred = self.literal.split('(')[0].strip()
+            fact_pred = fact.split('(')[0].strip()
+            lit_args = self.literal.split('(')[1][:-1].split(',')
+            fact_args = fact.split('(')[1][:-1].split(',')
+            n = len(lit_args)
+            m = len(fact_args)
+            if lit_pred == fact_pred and n == m:
+                self.facts.append(fact)
+
+    def __repr__(self):
+        """call to print displays this info
+        """
+
+        rep = ""
+        rep += str(self.literal)+"\n"
+        rep += str(self.bindings)+"\n"
+        rep += str(self.facts)+"\n"
+        return (rep)
+
+    def substitute_with_bindings(self,bindings):
+        """substitutes literal vars
+           with given bindings
+        """
+
+        n_chars = len(self.literal)
+        term = ['' for i in range(n_chars)]
+
+        for i in range(n_chars):
+            if self.literal[i] in bindings:
+                term[i] = bindings[self.literal[i]]
+            else:
+                term[i] = self.literal[i]
+
+        return (''.join(term))
+        
+
+    def substitute(self):
+        """substitutes literal vars
+           with bindings
+        """
+
+        n_chars = len(self.literal)
+        term = ['' for i in range(n_chars)]
+
+        for i in range(n_chars):
+            if self.literal[i] in self.bindings:
+                term[i] = self.bindings[self.literal[i]]
+            else:
+                term[i] = self.literal[i]
+
+        return (''.join(term))
+
+    def unify(self,term,fact,bindings):
+        """unification of two terms
+        """
+
+        n = len(term.split('(')[1][:-1].split(','))
+        term_args = term.split('(')[1][:-1].split(',')
+        fact_args = fact.split('(')[1][:-1].split(',')
+        for i in range(n):
+            if (not Prover.is_var(term_args[i])) and (not Prover.is_var(fact_args[i])):
+                if term_args[i] != fact_args[i]:
+                    return False
+            elif (Prover.is_var(term_args[i])) and (not Prover.is_var(fact_args[i])):
+                bindings[term_args[i]] = fact_args[i]
+            elif (not Prover.is_var(term_args[i])) and (Prover.is_var(fact_args[i])):
+                bindings[fact_args[i]] = term_args[i]
+        return bindings
+        
+
+    def search(self):
+        """searches facts after substituting
+           with bindings
+        """
+
+        term = self.substitute()
+        ##print ("searching:",term)
+        ##print ("in facts",self.facts)
+        ##input()
+        bindings = deepcopy(self.bindings)
+        found = False
+        for fact in self.facts:
+            found = self.unify(term,fact,bindings)
+            if found:
+                bound_vars = list(bindings.keys())
+                n_bound_vars = len(bound_vars)
+                for i in range(n_bound_vars):
+                    for j in range(i+1,n_bound_vars):
+                        if bindings[bound_vars[i]] == bindings[bound_vars[j]]:
+                            return False
+                self.facts.remove(self.substitute_with_bindings(bindings)) #THINK ABOUT THIS
+                break
+        return found
+
+class Prover(object):
+    """contains functions for proving theories
+    """
+
+    facts = []
+    rule = ""
+
+    @staticmethod
+    def is_var(argument):
+        """checks is argument is a variable
+           by checking if it starts with uppercase
+        """
+        if argument[0] in list(string.ascii_uppercase):
+            return True
+        return False
+
+    @staticmethod
+    def prove_rule(example,exists=True):
+        """checks if example satisfies
+           rule and against the facts
+        """
+
+        facts = Prover.facts
+        rule = Prover.rule
+        #assume example is true
+        proved = True
+        
+        #collect head variables and bind to example atoms
+        bindings = {}
+        head = rule.split(':-')[0].strip()
+        head_args = head.split('(')[1][:-1].split(',')
+        example_args = example.split('(')[1][:-1].split(',')
+        n_args = len(head_args)
+        for i in range(n_args):
+            if Prover.is_var(head_args[i]):
+                bindings[head_args[i]] = example_args[i]
+            else:
+                if head_args[i] != example_args[i]:
+                    bindings = {}
+                    break
+
+        #if no binding for head, example is false
+        if not bindings:
+            proved = False
+
+        #two different vars cant have same binding
+        bound_vars = list(bindings.keys())
+        n_bound_vars = len(bound_vars)
+        for i in range(n_bound_vars):
+            for j in range(i+1,n_bound_vars):
+                if bindings[bound_vars[i]] == bindings[bound_vars[j]]:
+                    return False
+        
+        #collect body literals
+        body = rule.split(':-')[1].strip()
+        body_literals = body.split(';')
+        pointer = 0
+        stack = [Proof_node(body_literals[0],bindings,facts)]
+        solutions = []
+        while stack:
+            node = stack[-1]
+            ##print (node)
+            ##input()
+            x = node.search()
+            ##print (x)
+            ##input()
+            if x:
+                if len(stack) != len(body_literals):
+                    new_node = Proof_node(body_literals[len(stack)],x,facts)
+                    stack.append(new_node)
+                else:
+                    if exists:
+                        return True
+                    solutions.append(x)
+                    stack.pop()
+            elif not x:
+                stack.pop()
+
+        if not solutions:
+            return False
+        return (solutions)
+        
+#============TEST-CASE-1=====================#
+'''
+facts = ['p(a,b,d,20)','p(a,b,c,20)','h(a,c,50)','h(a,d,50)']
+example = 'q(a,b)'
+rule = 'q(A,B) :- p(A,B,C,20);h(A,C,50)'
+Prover.facts = facts
+Prover.rule = rule
+solutions = Prover.prove_rule(example)
+print (solutions)
+'''
---
+++ TILDE.py
@@ -0,0 +1,346 @@
+from string import ascii_uppercase
+from itertools import product
+from copy import deepcopy
+from Prover import Prover
+from math import log2
+
+
+def entropy(bool_list):
+    """computes entropy of list of 1's and 0's
+    """
+
+    if (not bool_list) or (len(bool_list) == 1):
+        return 0
+    p1 = sum(bool_list)/len(bool_list)
+    p0 = 1-p1
+    return (-1*p1*log2(p1)-1*p0*log2(p0))
+
+class Node(object):
+    """implements a tree node with
+       information to expand the tree
+    """
+
+    #static variable to store target and target variables
+    target = None
+    typ = None
+    score = None
+    all_vars = list(ascii_uppercase)
+    target_pred = ''
+
+
+    @staticmethod
+    def set_target_pred(bk):
+        """sets the target predicate string
+           and initializes types for variables
+           based on mode specification
+        """
+
+        
+    
+    def __init__(self,facts,examples,bk,depth = 0,p="root",parent="root",var_types={}):
+        """stores data for finding best condition
+           background for restriction condition search
+           parent to obtain branch info
+           depth for tree depth control
+           'p' position to know if root, left or right child
+        """
+
+        self.facts = facts
+        self.examples = examples
+        self.best_condition = ""
+        self.parent = parent
+        self.bk = bk
+        self.depth = depth
+        self.p = p
+        self.var_types = var_types
+        if parent == "root":
+            Node.target_pred = Node.target+'('
+            for functor in bk:
+                if functor.split('(')[0] == Node.target:
+                    mode = functor.split('(')[1][:-1].split(',')
+                    n = len(mode)
+                    for i in range(n):
+                        variable = Node.all_vars[i]
+                        typ = mode[i][1:]
+                        Node.target_pred += variable+','
+                        self.var_types[variable] = typ
+                    break
+            Node.target_pred = Node.target_pred[:-1]+')'
+
+    def find_test_conditions(self,bk):
+        """generate body literals in clause to score
+           based on mode specifications in backgroud
+        """
+
+        test_conditions = []
+        children_var_types = deepcopy(self.var_types)
+
+        for functor in bk:
+
+            #target mode specification skip
+            if functor.split('(')[0] == Node.target:
+                continue
+
+            #for others generate literals based on mode
+            pred = functor.split('(')[0]
+            mode = functor.split('(')[1][:-1].split(',')
+            n = len(mode)
+            args = [[] for i in range(n)]
+            for i in range(n):
+                spec = mode[i]
+
+                #if '+' then instantiate with seen var of same type
+                if spec[0] == '+':
+                    typ = spec[1:]
+
+                    #assume var of similar type doesn't exist
+                    exists = False
+                    
+                    for var in self.var_types:
+                        if self.var_types[var] == typ:
+                            exists = True
+                            args[i].append(var)
+                            children_var_types[var] = typ
+
+                    #if no variable of that type in current clause
+                    if not exists:
+
+                        #assume variable of that type not seen in some previous mode
+                        seen = False
+                        for var in Node.all_vars:
+                            if (var in children_var_types) and (children_var_types[var] == typ):
+                                seen = True
+                                args[i].append(var)
+                                break
+
+                        if not seen:
+                            for new_var in Node.all_vars:
+                                if new_var not in children_var_types:
+                                    args[i].append(new_var)
+                                    children_var_types[new_var] = typ
+                                    break
+
+                #if '-' instantiate with new var not in clause
+                elif spec[0] == '-':
+                    typ = spec[1:]
+
+                    seen = False
+                    for var in Node.all_vars:
+                        if (var in children_var_types) and (children_var_types[var] == typ) and (var not in self.var_types):
+                            seen = True
+                            args[i].append(var)
+                            break
+
+                    if not seen:
+                        for new_var in Node.all_vars:
+                            if new_var not in children_var_types:
+                                args[i].append(new_var)
+                                children_var_types[new_var] = typ
+                                break
+                
+                #if '#' collect all constants from facts at this position
+                elif spec[0] == '#':
+                    for fact in self.facts:
+                        if fact.split('(')[0] == pred:
+                            fact_args = fact.split('(')[1][:-1].split(',')
+                            if fact_args[i] not in args[i]:
+                                args[i].append(fact_args[i])
+
+            combinations = list(product(*args))
+            for combination in combinations:
+                literal = pred+'('+','.join(list(combination))+')'
+                if (literal not in test_conditions):
+                    test_conditions.append((literal,functor))
+
+
+        return ((test_conditions,children_var_types))
+
+    def __repr__(self):
+        """call to print outputs this
+           this will be clause upto the node
+        """
+
+        clause = Node.target_pred+':-'
+        if self.parent == "root":
+            return (clause)
+        pointer = self
+        while pointer.parent!="root":
+            if pointer.p == "left":
+                clause += pointer.parent.best_condition+','
+            else:
+                clause += ""
+            pointer = pointer.parent
+
+        if clause[-1] == ',':
+            clause = clause[:-1]
+        return (clause)
+
+    def score_clause(self,clause):
+        """multiprocessing scoring function
+        """
+        
+        examples = self.examples
+        data = self.facts
+        example_list = list(examples.keys())
+        n = len(example_list)
+        true_examples = {}
+        false_examples = {}
+        
+        Prover.rule = clause
+        Prover.facts = data
+        
+        result = []
+        for example in example_list:
+            result.append(Prover.prove_rule(example))
+        
+        for i in range(n):
+            if (not not result[i]):
+                true_examples[example_list[i]] = examples[example_list[i]]
+            else:
+                false_examples[example_list[i]] = examples[example_list[i]]
+        nt = len(true_examples)
+        nf = len(false_examples)
+        n = nt+nf
+        if Node.score == "IG":
+            left_entropy = entropy(list(true_examples.values()))
+            right_entropy = entropy(list(false_examples.values()))
+            ig = (nt*left_entropy)/n + (nf*right_entropy)/n
+
+        return ((ig,true_examples,false_examples))
+        
+    def expand(self):
+        """scores all clauses with test conditions
+           and expands on best test
+        """
+        print ("expanding node at position "+str(self.p)+" and depth "+str(self.depth))
+        conditions_and_var_types = self.find_test_conditions(self.bk)
+        child_bk = []
+        test_conditions_and_modes = conditions_and_var_types[0]
+        test_conditions = [item[0] for item in test_conditions_and_modes]
+        modes = [item[1] for item in test_conditions_and_modes]
+        children_var_types = conditions_and_var_types[1]
+        clauses = []
+        for test_condition in test_conditions:
+            if self.parent != 'root':
+                if str(self)[-1] == ')':
+                    clause = str(self)+';'+test_condition
+                else:
+                    clause = str(self)+test_condition
+                clauses.append(clause)
+            else:
+                clause = str(self)+test_condition
+                clauses.append(clause)
+                
+        #score all clauses and extract minimum
+        result = []
+        for clause in clauses:
+            result.append(self.score_clause(clause))
+        scores = [r[0] for r in result]
+        min_score = min(scores)
+        index = scores.index(min_score)
+        self.best_condition = test_conditions[index]
+        print ("best_split found is "+str(self.best_condition)+" with score "+str(min_score))
+        input()
+        mode_to_remove = modes[index]
+
+        #remove tested mode from child bk
+        for mode in self.bk:
+            if mode != mode_to_remove:
+                child_bk.append(mode)
+
+        #get left children variable types to pass to left node
+        child_var_types = {}
+        for var in children_var_types:
+            if var in self.best_condition.split('(')[1][:-1].split(','):
+                child_var_types[var] = children_var_types[var]
+
+        left_child = Node(self.facts,result[index][1],child_bk,depth = self.depth+1,p="left",parent=self,var_types=child_var_types)
+        right_child = Node(self.facts,result[index][2],child_bk,depth = self.depth+1,p="right",parent=self,var_types=child_var_types)
+        
+        return (min_score,left_child,right_child)
+
+class TILDE(object):
+    """implements top-down induction
+       of logical decision tree
+    """
+
+    def __init__(self,typ="classification",score="IG",max_depth = 2):
+        """initializes type and scoring criteria
+        """
+
+        self.typ = typ
+        self.score = score
+        self.max_depth = max_depth
+        self.clauses = []
+    
+    def learn(self,facts,bk,target,pos=[],neg=[],examples={}):
+        """learns TILDE tree from data
+           examples and background info
+        """
+
+        #set values to binary if classification
+        #in regression assumed dictionary input like {a(b): <value>}
+        if self.typ == "classification":
+            examples = {}
+            for ex in pos:
+                examples[ex] = 1
+            for ex in neg:
+                examples[ex] = 0
+
+        #assign tree target
+        Node.target = target
+        Node.typ = self.typ
+        Node.score = self.score
+        
+        #depth first search expansion
+        stack = [Node(facts,examples,bk)]
+        
+        while stack:
+            top_node = stack.pop()
+            info = top_node.expand()
+            score = info[0]
+            left_node = info[1]
+            right_node = info[2]
+
+            #check if score 0 or max depth reached
+            if (top_node.depth + 1 == self.max_depth) or round(score,5) == 0.0:
+                left_node_clause = str(left_node)
+                if self.typ == "classification":
+                    self.clauses.append((left_node_clause,sum(list(left_node.examples.values()))/len(left_node.examples)))
+                right_node_clause = str(right_node)
+                if self.typ == "classification":
+                    self.clauses.append((right_node_clause,sum(list(right_node.examples.values()))/len(right_node.examples)))
+
+            #if not push right and left child for expansion if there are examples there
+            else:
+                if right_node.examples:
+                    stack.append(right_node)
+                if left_node.examples:
+                    stack.append(left_node)
+
+        print (self.clauses)
+        input()
+
+    def infer(self,data,example):
+        """infers value of example
+           from learned target tree
+        """
+
+        Prover.facts = data
+        for clause in self.clauses:
+            Prover.rule = clause[0]
+            value = clause[1]
+            if Prover.prove_rule(example):
+                return value
+            
+            
+#===========TEST-CASE-1=================
+if __name__ == '__main__':
+    x = TILDE()
+    x.learn(['o(m1,d1)','r(m1,w1,st)','o(m2,d2)','r(m2,w2,st)','o(m3,d3)','r(m3,w3,st)','o(m4,d4)','r(m4,w4,lt)','r(m5,w5,st)','r(m6,w6,lt)','r(m7,w7,lt)'],
+            ['h(+man)','o(+man,-dog)','r(+man,-woman,#term)'],
+            'h',
+            pos=['h(m1)','h(m2)','h(m4)','h(m6)'],
+            neg=['h(m3)','h(m5)','h(m7)'])
+    print (x.infer(['o(m1,d1)','r(m1,w1,st)','o(m2,d2)','r(m2,w2,st)','o(m3,d3)','r(m3,w3,st)','o(m4,d4)','r(m4,w4,lt)','r(m5,w5,st)','r(m6,w6,lt)','r(m7,w7,lt)'],
+                   'h(m1)'))
